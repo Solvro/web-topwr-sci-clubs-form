f348dd82fa14e1586460f6eaf670e221e6f2337e
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-23 00:01:02 +0200

    feat: better htm editor

--- lib/features/html_editor_field/html_field.dart
+++ lib/features/html_editor_field/html_field.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quill_html_editor/quill_html_editor.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:reactive_forms_annotations/reactive_forms_annotations.dart';
 
 import '../../config/config.dart';
 import '../../theme/app_theme.dart';
 
-final _quillControler = Provider((ref) => QuillEditorController());
+final _quillControler = Provider((ref) => HtmlEditorController());
 
 class HtmlField extends ConsumerWidget {
   const HtmlField(this.title, this.formControl, {super.key});
@@ -36,32 +36,34 @@ class HtmlField extends ConsumerWidget {
               ),
               tileColor: context.colorTheme.greyLight,
             ),
-            ToolBar(
-              toolBarColor: context.colorTheme.greyLight,
-              activeIconColor: context.colorTheme.orangePomegranade,
-              controller: controller,
-            ),
-            QuillHtmlEditor(
-              text: "<h1>Hello</h1>To przykład twojego super opisu koła 😊",
-              controller: controller,
-              isEnabled: true,
-              minHeight: 300,
-              textStyle: context.textTheme.body,
-              hintTextStyle: context.textTheme.lightTitle,
-              hintTextAlign: TextAlign.start,
-              padding: FormFieldConfig.padding,
-              backgroundColor: context.colorTheme.whiteSoap,
-              onFocusChanged: (hasFocus) {
-                if (!hasFocus) formControl?.markAsTouched();
-              },
-              onTextChanged: (text) => formControl?.updateValue(text),
-              // onEditingComplete: (s) => formControl.,
-              loadingBuilder: (context) {
-                return const Center(
-                    child: CircularProgressIndicator(
-                  strokeWidth: 0.4,
-                ));
-              },
+            HtmlEditor(
+              controller: controller, //required
+              htmlEditorOptions: const HtmlEditorOptions(
+                initialText:
+                    "<h1>Hello</h1>To przykład twojego super opisu koła 😊",
+              ),
+              otherOptions: const OtherOptions(height: 400),
+              htmlToolbarOptions: const HtmlToolbarOptions(
+                defaultToolbarButtons: [
+                  StyleButtons(),
+                  FontButtons(clearAll: false),
+                  ColorButtons(),
+                  ListButtons(listStyles: false),
+                  ParagraphButtons(
+                    textDirection: false,
+                    lineHeight: false,
+                    caseConverter: false,
+                  ),
+                  InsertButtons(
+                    video: false,
+                    audio: false,
+                    table: false,
+                    hr: false,
+                    otherFile: false,
+                  ),
+                ],
+              ),
+              callbacks: Callbacks(),
             ),
           ],
         ),
