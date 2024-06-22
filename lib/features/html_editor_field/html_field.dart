530e4522e506fa31186835698788b21c220f8b73
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-23 00:39:20 +0200

    feat: better html editor

--- lib/features/html_editor_field/html_field.dart
+++ lib/features/html_editor_field/html_field.dart
@@ -5,6 +5,8 @@ import 'package:reactive_forms_annotations/reactive_forms_annotations.dart';
 
 import '../../config/config.dart';
 import '../../theme/app_theme.dart';
+import '../../utils/context_extensions.dart';
+import '../form/widgets/form_subsection.dart';
 
 final _quillControler = Provider((ref) => HtmlEditorController());
 
@@ -18,56 +20,70 @@ class HtmlField extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final controller = ref.watch(_quillControler);
 
-    return Padding(
-      padding: FormFieldConfig.padding,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: context.colorTheme.blackMirage,
+    return FormSubsection(
+      title: title,
+      buildChildren: (setError) => [
+        HtmlEditor(
+          controller: controller, //required
+          htmlEditorOptions: HtmlEditorOptions(
+            initialText: formControl?.value,
+            hint: "<p>${context.localize.form_sci_desc_hint}</p>",
           ),
-          borderRadius: const BorderRadius.all(FormFieldConfig.radius),
+          otherOptions: const OtherOptions(height: 400),
+          htmlToolbarOptions: const HtmlToolbarOptions(
+            defaultToolbarButtons: [
+              StyleButtons(),
+              FontButtons(clearAll: false),
+              ColorButtons(),
+              ListButtons(listStyles: false),
+              ParagraphButtons(
+                textDirection: false,
+                lineHeight: false,
+                caseConverter: false,
+              ),
+              InsertButtons(
+                video: false,
+                audio: false,
+                table: false,
+                hr: false,
+                otherFile: false,
+              ),
+            ],
+          ),
+          callbacks: Callbacks(onChangeContent: (p0) {
+            debugPrint(p0);
+            formControl?.updateValue(p0);
+            formControl?.updateValueAndValidity();
+            setError(false);
+          }, onInit: () {
+            controller.setFullScreen();
+          }),
         ),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text(
-                title,
-                style: context.textTheme.lightTitle,
-              ),
-              tileColor: context.colorTheme.greyLight,
-            ),
-            HtmlEditor(
-              controller: controller, //required
-              htmlEditorOptions: const HtmlEditorOptions(
-                initialText:
-                    "<h1>Hello</h1>To przykład twojego super opisu koła 😊",
-              ),
-              otherOptions: const OtherOptions(height: 400),
-              htmlToolbarOptions: const HtmlToolbarOptions(
-                defaultToolbarButtons: [
-                  StyleButtons(),
-                  FontButtons(clearAll: false),
-                  ColorButtons(),
-                  ListButtons(listStyles: false),
-                  ParagraphButtons(
-                    textDirection: false,
-                    lineHeight: false,
-                    caseConverter: false,
-                  ),
-                  InsertButtons(
-                    video: false,
-                    audio: false,
-                    table: false,
-                    hr: false,
-                    otherFile: false,
-                  ),
-                ],
-              ),
-              callbacks: Callbacks(),
-            ),
-          ],
-        ),
-      ),
+      ],
+      onInitState: () {},
     );
+
+    // return Padding(
+    //   padding: FormFieldConfig.padding,
+    //   child: Container(
+    //     decoration: BoxDecoration(
+    //       border: Border.all(
+    //         color: context.colorTheme.blackMirage,
+    //       ),
+    //       borderRadius: const BorderRadius.all(FormFieldConfig.radius),
+    //     ),
+    //     child: Column(
+    //       children: [
+    //         ListTile(
+    //           title: Text(
+    //             ,
+    //             style: context.textTheme.lightTitle,
+    //           ),
+    //           tileColor: context.colorTheme.greyLight,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
