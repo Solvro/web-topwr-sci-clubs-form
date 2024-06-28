a72aaade51a2fa50b5da24cb40638b1dfef66720
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-28 20:55:22 +0200

    feat: generate native splash screen

--- lib/features/topwr_mockup/config/ui_config.dart
+++ lib/features/topwr_mockup/config/ui_config.dart
@@ -10,7 +10,7 @@ abstract class AppBarConfig {
 
 abstract class SplashScreenConfig {
   static const logoAssetName = "assets/splash_screen/splash_logo.png";
-  static const additionalWaitDuration = Duration(seconds: 1);
+  static const additionalWaitDuration = Duration(milliseconds: 0);
   static const animationDuration = Duration(milliseconds: 800);
   static const androidSplashSizeInDp = 288.0;
   static const androidNativeSplashColor = Color(0xFFEE6644);
