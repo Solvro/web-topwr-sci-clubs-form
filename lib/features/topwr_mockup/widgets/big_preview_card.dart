0dcb5485a0395d97ce51c66832e02fe0ffbeaeb1
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-30 00:07:44 +0200

    fix: random padding

--- lib/features/topwr_mockup/widgets/big_preview_card.dart
+++ lib/features/topwr_mockup/widgets/big_preview_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../config/ui_config.dart';
 import '../../../theme/app_theme.dart';
 import '../../../utils/context_extensions.dart';
+import '../config/ui_config.dart';
 import 'date_chip.dart';
 import 'dual_text_max_lines.dart';
 import 'my_cached_image.dart';
@@ -69,13 +69,15 @@ class BigPreviewCard extends StatelessWidget {
                   const Spacer(),
                   MaterialButton(
                     elevation: 1,
-                    padding: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.all(16),
                     onPressed: onClick,
                     color: context.colorTheme.orangePomegranade,
                     textColor: context.colorTheme.whiteSoap,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(4)),
-                    child: Text(context.localize.read_more),
+                    child: Text(
+                      context.localize.read_more,
+                    ),
                   ),
                 ],
               ),
