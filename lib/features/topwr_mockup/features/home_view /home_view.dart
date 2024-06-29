131d25562d699402f66b4f085b64568f67340301
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-29 23:11:43 +0200

    fix: scrolldown showcase

--- lib/features/topwr_mockup/features/home_view /home_view.dart
+++ lib/features/topwr_mockup/features/home_view /home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:separate/separate.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import '../../../../theme/app_theme.dart';
 import '../../../../utils/context_extensions.dart';
@@ -9,7 +8,6 @@ import '../../config/ui_config.dart';
 import 'widgets/logo_app_bar.dart';
 import 'widgets/placeholders/horizontal_placeholder.dart';
 import 'widgets/placeholders/subsection_placeholder.dart';
-
 import 'widgets/study_circles_section.dart';
 
 class HomeView extends StatelessWidget {
@@ -17,55 +15,52 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final scrollDownKey = GlobalKey();
+    final itemScrollController = ItemScrollController();
+    const studyCirclesSections = StudyCirclesSection();
     final List<Widget> sections = [
-      // const Greeting(),
       const HorizontalPlaceholder(),
-      // const ExamSessionCountdown(),
       const HorizontalPlaceholder(),
-      // const ParkingSection(),
       SubsectionPlaceholder(
         title: context.localize.parkings_title,
         isMapActionString: true,
         tabDestination: NavBarEnum.parkings,
       ),
-      // const NewsSection(),
       SubsectionPlaceholder(
         title: context.localize.whats_up,
         big: true,
         tabDestination: NavBarEnum.info,
       ),
-      // const BuildingsSection(),
       SubsectionPlaceholder(
         title: context.localize.buildings_title,
         isMapActionString: true,
         tabDestination: NavBarEnum.mapp,
       ),
-      StudyCirclesSection(key: scrollDownKey),
-      // const DepartmentSection(),
+      studyCirclesSections,
       SubsectionPlaceholder(
         title: context.localize.departments,
         tabDestination: NavBarEnum.faculties,
       ),
     ];
 
-    Future.delayed(Durations.extralong4, () {
-      Scrollable.ensureVisible(
-        scrollDownKey.currentContext!,
-        // alignment: widget.alignment,
-        duration: Durations.short3,
+    Future.delayed(Durations.medium2, () {
+      itemScrollController.scrollTo(
+        index: sections.indexOf(studyCirclesSections),
+        duration: Durations.medium4,
+        alignment: 0.01,
+        curve: Curves.decelerate,
       );
     });
+
     return Scaffold(
       backgroundColor: context.colorTheme.whiteSoap,
       appBar: LogoAppBar(context),
-      body: ListView(
+      body: ScrollablePositionedList.separated(
+        itemScrollController: itemScrollController,
         padding: const EdgeInsets.only(bottom: 48),
-        children: sections.separate(
-          (i, e0, e1) => const SizedBox(
-            height: HomeScreenConfig.paddingMedium,
-          ),
-        ),
+        itemCount: sections.length,
+        itemBuilder: (BuildContext context, int index) => sections[index],
+        separatorBuilder: (context, index) =>
+            const SizedBox(height: HomeScreenConfig.paddingMedium),
       ),
     );
   }
