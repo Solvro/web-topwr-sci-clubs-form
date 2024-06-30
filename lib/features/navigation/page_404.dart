f247e4d263a7d2231d292703001285e8bfde0f8d
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-30 17:03:19 +0200

    feat: 404 page

--- lib/features/navigation/page_404.dart
+++ lib/features/navigation/page_404.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../config/config.dart';
+import '../../../theme/app_theme.dart';
+import '../../../utils/context_extensions.dart';
+import '../auth/widgets/pure_app_bar.dart';
+import '../topwr_mockup/widgets/my_text_button.dart';
 
 class Page404 extends StatelessWidget {
   const Page404({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("404 page");
+    return Scaffold(
+      appBar: PureWebAppBar(context),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            Text(
+              context.localize.text404,
+              style: context.greetingTheme.boldTextStyle,
+              textAlign: TextAlign.justify,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: MyTextButton(
+                isOrange: true,
+                actionTitle: context.localize.take_me_home,
+                onClick: () {
+                  context.go("/");
+                },
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: const Alignment(0, -0.6),
+                child: Container(
+                  constraints:
+                      const BoxConstraints(maxHeight: 400, minHeight: 10),
+                  padding: const EdgeInsets.all(40.0),
+                  child: SvgPicture.asset(
+                    UndrawConfig.p404,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
