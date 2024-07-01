f4382e40d424f00c274c19d24b8f7f9704cda291
Szymon Kowaliński <szymon@kowalinski.dev>
2024-07-01 10:26:14 +0200

    fix: page padding

--- lib/features/post_form_pages/saving_page.dart
+++ lib/features/post_form_pages/saving_page.dart
@@ -17,22 +17,25 @@ class SavingPage extends StatelessWidget {
       appBar: PureWebAppBar(context),
       body: Align(
         alignment: const Alignment(0, -0.5),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              context.localize.saving,
-              style: context.greetingTheme.boldTextStyle,
-              textAlign: TextAlign.justify,
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                context.localize.saving,
+                style: context.greetingTheme.boldTextStyle,
+                textAlign: TextAlign.justify,
+              ),
+              const CircularProgressIndicator(),
+              Text(
+                context.localize.wait_here,
+                style: context.textTheme.headline,
+                textAlign: TextAlign.justify,
+              ),
+            ].separate(
+              (i, e0, e1) => const SizedBox(height: 60),
             ),
-            const CircularProgressIndicator(),
-            Text(
-              context.localize.wait_here,
-              style: context.textTheme.headline,
-              textAlign: TextAlign.justify,
-            ),
-          ].separate(
-            (i, e0, e1) => const SizedBox(height: 60),
           ),
         ),
       ),
