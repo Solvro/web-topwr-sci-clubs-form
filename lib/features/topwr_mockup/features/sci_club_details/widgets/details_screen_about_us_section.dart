f31d178d2f1ab411b4e4c0abf3b1a8bcf69e0bd8
Szymon Kowaliński <szymon@kowalinski.dev>
2024-07-14 13:02:56 +0200

    feat: change link to pomegranade

--- lib/features/topwr_mockup/features/sci_club_details/widgets/details_screen_about_us_section.dart
+++ lib/features/topwr_mockup/features/sci_club_details/widgets/details_screen_about_us_section.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+
 import '../../../../../theme/app_theme.dart';
 import '../../../../../utils/context_extensions.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import '../../../../../utils/convert_html.dart';
+import '../../../../../utils/launch_url_util.dart';
 
 class AboutUsSection extends StatelessWidget {
   const AboutUsSection({super.key, required this.text});
@@ -20,7 +23,14 @@ class AboutUsSection extends StatelessWidget {
                 Text(context.localize.about_us,
                     style: context.textTheme.headline),
                 const SizedBox(height: 16),
-                HtmlWidget(text),
+                HtmlWidget(
+                  text,
+                  customStylesBuilder: (element) =>
+                      context.customStylesBuilder(element),
+                  onTapUrl: (url) async {
+                    return LaunchUrlUtil.launch(url);
+                  },
+                ),
               ],
             ),
           );
